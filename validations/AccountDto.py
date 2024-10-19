8c67dcc8e7f4879f112721dd0c537dfc47149c51
patrickvillarroel <130080309+patrickvillarroel@users.noreply.github.com>
2024-10-19 15:43:15 -0500

    perf: add none default

--- validations/AccountDto.py
+++ validations/AccountDto.py
@@ -6,41 +6,41 @@ from pydantic import BaseModel, Field, ConfigDict
 
 
 class PermisoDto(BaseModel):
-    id: Optional[Annotated[int, Field(...)]]
+    id: Optional[Annotated[int, Field(None)]]
     nombre: Annotated[str, Field(max_length=100, min_length=1)]
     descripcion: Optional[Annotated[str, Field(max_length=100, min_length=1)]]
-    created_at: Optional[Annotated[datetime, Field(...)]]
-    updated_at: Optional[Annotated[datetime, Field(...)]]
+    created_at: Optional[Annotated[datetime, Field(None)]]
+    updated_at: Optional[Annotated[datetime, Field(None)]]
 
     model_config = ConfigDict(frozen=True)
 
 
 class RolDto(BaseModel):
-    id: Optional[Annotated[int, Field(...)]]
+    id: Optional[Annotated[int, Field(None)]]
     nombre: Optional[Annotated[str, Field(max_length=100, min_length=1)]]
     descripcion: Optional[Annotated[str, Field(max_length=100, min_length=1)]]
     permisos: Optional[Annotated[List[PermisoDto], Field(min_length=1, frozen=True)]]
-    created_at: Optional[Annotated[datetime, Field(...)]]
-    updated_at: Optional[Annotated[datetime, Field(...)]]
+    created_at: Optional[Annotated[datetime, Field(None)]]
+    updated_at: Optional[Annotated[datetime, Field(None)]]
 
     model_config = ConfigDict(frozen=True)
 
 
 class LoginInDto(BaseModel):
-    username: Annotated[str, Field(..., min_length=1, max_length=50)]
-    password: Annotated[str, Field(..., min_length=8, max_length=70)]
+    username: Annotated[str, Field(None, min_length=1, max_length=50)]
+    password: Annotated[str, Field(None, min_length=8, max_length=70)]
 
 
 class UsuarioDto(BaseModel):
-    id: Optional[Annotated[UUID, Field(...)]]
+    id: Optional[Annotated[UUID, Field(None)]]
     username: Optional[Annotated[str, Field(min_length=1)]]
     password: Optional[Annotated[str, Field(min_length=8, max_length=70)]] = None
-    created_at: Optional[Annotated[datetime, Field(...)]]
-    updated_at: Optional[Annotated[datetime, Field(...)]]
-    last_used: Optional[Annotated[datetime, Field(...)]]
-    roles: Annotated[List[RolDto], Field(..., min_length=1, frozen=True)]
-    cedula: Optional[Annotated[str, Field(...,
+    created_at: Optional[Annotated[datetime, Field(None)]]
+    updated_at: Optional[Annotated[datetime, Field(None)]]
+    last_used: Optional[Annotated[datetime, Field(None)]]
+    roles: Annotated[List[RolDto], Field(None, min_length=1, frozen=True)]
+    cedula: Optional[Annotated[str, Field(None,
                                           pattern="^(PE|E|N|[23456789](?:AV|PI)?|1[0123]?(?:AV|PI)?)-(\\d{1,4})-(\\d{1,6})$",
                                           max_length=15)]] = None
-    pasaporte: Optional[Annotated[str, Field(..., pattern="^[A-Z0-9]{5,20}$", max_length=20)]] = None
-    licencia_fabricante: Optional[Annotated[str, Field(..., pattern="^.+/DNFD$", max_length=50)]] = None
+    pasaporte: Optional[Annotated[str, Field(None, pattern="^[A-Z0-9]{5,20}$", max_length=20)]] = None
+    licencia_fabricante: Optional[Annotated[str, Field(None, pattern="^.+/DNFD$", max_length=50)]] = None
--- validations/PacienteDto.py
+++ validations/PacienteDto.py
@@ -12,26 +12,27 @@ from validations.DireccionesDto import DireccionDto
 
 
 class PacienteDto(BaseModel):
-    id: UUID
-    cedula: Optional[Annotated[str, Field(...,
+    id: Optional[UUID] = None
+    cedula: Optional[Annotated[str, Field(None,
                                           pattern="^(PE|E|N|[23456789](?:AV|PI)?|1[0123]?(?:AV|PI)?)-(\\d{1,4})-(\\d{1,6})$",
                                           max_length=15)]]
-    pasaporte: Optional[Annotated[str, Field(..., pattern="^[A-Z0-9]{5,20}$", max_length=20)]]
-    nombre: Optional[Annotated[str, Field(..., max_length=100)]]
-    nombre2: Optional[Annotated[str, Field(..., max_length=100)]]
-    apellido1: Optional[Annotated[str, Field(..., max_length=100)]]
-    apellido2: Optional[Annotated[str, Field(..., max_length=100)]]
-    correo: Optional[Annotated[EmailStr, Field(..., max_length=254)]]
+    pasaporte: Optional[Annotated[str, Field(None, pattern="^[A-Z0-9]{5,20}$", max_length=20)]]
+    nombre: Optional[Annotated[str, Field(None, max_length=100)]]
+    nombre2: Optional[Annotated[str, Field(None, max_length=100)]]
+    apellido1: Optional[Annotated[str, Field(None, max_length=100)]]
+    apellido2: Optional[Annotated[str, Field(None, max_length=100)]]
+    correo: Optional[Annotated[EmailStr, Field(None, max_length=254)]]
     telefono: Optional[
-        Annotated[PhoneNumber, Field(..., max_length=100, description="Número de teléfono en formato '+5076000000'")]]
-    fecha_nacimiento: Annotated[datetime, Field(...)]
-    edad: Optional[Annotated[int, Field(...)]]
-    sexo: Annotated[str, Field(..., max_length=1, pattern="^[MFX]$",
+        Annotated[PhoneNumber, Field(None, max_length=100, description="Número de teléfono en formato '+5076000000'")]]
+    fecha_nacimiento: Annotated[date, Field(None)]
+    edad: Optional[Annotated[int, Field(None)]]
+    sexo: Annotated[str, Field(None, max_length=1, pattern="^[MFX]$",
                                description="Sexo permitos= M masculino, F femenino y X otros")]
     estado: Annotated[str, Field("NO_VALIDADO", max_length=50)]
     disabled: Annotated[bool, Field(False)]
-    direccion: Optional[Annotated[DireccionDto, Field(...)]]
-    usuario: Optional[Annotated[UsuarioDto, Field(...)]]
+    direccion: Optional[Annotated[DireccionDto, Field(None)]]
+    usuario: Optional[Annotated[UsuarioDto, Field(None)]]
+
 
     @field_validator('telefono')
     def validate_telefono(cls, v: str) -> str:
@@ -42,7 +43,7 @@ class PacienteDto(BaseModel):
             raise ValueError("Error al validar número de teléfono en formato E.164")
 
     @field_validator('fecha_nacimiento')
-    def validate_fecha_nacimiento(cls, v: datetime) -> datetime:
-        if v.date() > date.today():
+    def validate_fecha_nacimiento(cls, v: date) -> date:
+        if v > date.today():
             raise ValueError("La fecha debe ser hoy o en el pasado")
         return v
